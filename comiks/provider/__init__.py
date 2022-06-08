f9d3700f688cca79a7c94b8805ebe213bcbc85bc
BOOM <23080994+b0oml@users.noreply.github.com>
2022-06-08 19:57:35 +0200

    Add Bitbucket provider

--- comiks/provider/__init__.py
+++ comiks/provider/__init__.py
@@ -1,7 +1,9 @@
+from comiks.provider.bitbucket import BitbucketProvider
 from comiks.provider.github import GithubProvider
 from comiks.provider.gitlab import GitlabProvider
 
 PROVIDERS = [
     GithubProvider,
     GitlabProvider,
+    BitbucketProvider,
 ]
---
+++ comiks/provider/bitbucket.py
@@ -0,0 +1,45 @@
+import requests
+
+from comiks.model import UserInfos, Repository
+from comiks.provider.base import Provider
+
+
+class BitbucketProvider(Provider):
+
+    name = 'Bitbucket'
+    url = 'bitbucket.org'
+    tags = ['bitbucket']
+
+    def __get(self, endpoint, params=None):
+        return requests.get(
+            f'https://bitbucket.org/api/2.0{endpoint}',
+            params=params or {},
+            auth=(
+                self.config.get('username'),
+                self.config.get('app_password'),
+            ),
+        ).json()
+
+    def get_user_infos(self, username):
+        resp = self.__get(f'/workspaces/{username}')
+
+        if resp.get('type') == 'error':
+            return None
+
+        return UserInfos(
+            username=resp['slug'],
+            display_name=resp['name'],
+            identifier=resp['uuid'],
+        )
+
+    def get_repositories(self, user_infos):
+        repos = self.__get(f'/repositories/{user_infos.username}')
+        for repo in repos.get('values', []):
+            if repo['type'] == 'repository':
+                repo_name = ' / '.join(repo['full_name'].split('/'))
+                repo_url = [
+                    link['href']
+                    for link in repo['links']['clone']
+                    if link['name'] == 'https'
+                ][0]
+                yield Repository(repo_name, repo_url)
